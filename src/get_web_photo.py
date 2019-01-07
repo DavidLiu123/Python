3c68cff2b0f75e933c5edf26261906e0f579b83e
Liu David（刘大伟） <davidliu@asrmicro.com>
2019-01-07 09:46:05 +0800

    modify tag and class content for new HTML

--- src/get_web_photo.py
+++ src/get_web_photo.py
@@ -13,30 +13,30 @@ class BeautifulPicture():
         print('开始网页get请求')
         r = self.request(self.web_url)
         print('开始获取所有a标签')
-        all_a = BeautifulSoup(r.text, 'lxml').find_all('a', class_='cV68d')  #获取网页中的class为cV68d的所有a标签
+        all_a = BeautifulSoup(r.text, 'lxml').find_all('img', class_='_2zEKz')  #获取网页中的class为cV68d的所有a标签
         print('开始创建文件夹')
         self.mkdir(self.folder_path)  #创建文件夹
         print('开始切换文件夹')
         os.chdir(self.folder_path)   #切换路径至上面创建的文件夹
 
         for a in all_a: #循环每个标签，获取标签中图片的url并且进行网络请求，最后保存图片
-            img_str = a['style'] #a标签中完整的style字符串
+            img_str = a['src'] #a标签中完整的style字符串
             print('a标签的style内容是：', img_str)
-            first_pos = img_str.index('"') + 1
-            second_pos = img_str.index('"',first_pos)
-            img_url = img_str[first_pos: second_pos] #使用Python的切片功能截取双引号之间的内容
-            #获取高度和宽度的字符在字符串中的位置
-            width_pos = img_url.index('&w=')
-            height_pos = img_url.index('&q=')
-            width_height_str = img_url[width_pos : height_pos] #使用切片功能截取高度和宽度参数，后面用来将该参数替换掉
-            print('高度和宽度数据字符串是：', width_height_str)
-            img_url_final = img_url.replace(width_height_str, '')  #把高度和宽度的字符串替换成空字符
-            print('截取后的图片的url是：', img_url_final)
+            # first_pos = img_str.index('"') + 1
+            # second_pos = img_str.index('"',first_pos)
+            # img_url = img_str[first_pos: second_pos] #使用Python的切片功能截取双引号之间的内容
+            # #获取高度和宽度的字符在字符串中的位置
+            # width_pos = img_url.index('&w=')
+            # height_pos = img_url.index('&q=')
+            # width_height_str = img_url[width_pos : height_pos] #使用切片功能截取高度和宽度参数，后面用来将该参数替换掉
+            # print('高度和宽度数据字符串是：', width_height_str)
+            # img_url_final = img_url.replace(width_height_str, '')  #把高度和宽度的字符串替换成空字符
+            # print('截取后的图片的url是：', img_url_final)
             #截取url中参数前面、网址后面的字符串为图片名
-            name_start_pos = img_url.index('photo')
-            name_end_pos = img_url.index('?')
-            img_name = img_url[name_start_pos : name_end_pos]
-            self.save_img(img_url_final, img_name) #调用save_img方法来保存图片
+            name_start_pos = img_str.index('photo')
+            name_end_pos = img_str.index('?')
+            img_name = img_str[name_start_pos : name_end_pos]
+            self.save_img(img_str, img_name) #调用save_img方法来保存图片
 
     def save_img(self, url, name): ##保存图片
         print('开始请求图片地址，过程会有点长...')
